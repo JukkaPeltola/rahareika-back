a785f2a8e219f7062216f8966a87493ab9588fe6
JukkaPeltola <jukka.peltola@mbnet.fi>
2018-09-24 14:24:58 +0300

    toimimaton xml parse

--- rahareika-back/Controllers/ValuesController.cs
+++ rahareika-back/Controllers/ValuesController.cs
@@ -3,16 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
+using System.Xml;
+using Newtonsoft.Json;
+using System.IO;
+
 
 namespace rahareika_back.Controllers
 {
     public class ValuesController : ApiController
     {
+        private static readonly HttpClient client = new HttpClient();
         // GET api/values
-        public IEnumerable<string> Get()
+        public async Task Get()
         {
-            return new string[] { "value1", "value2" };
+            XmlDocument responseFromBankXml = new XmlDocument();
+            var response = await client.GetAsync($"https://sdw-wsrest.ecb.europa.eu/service/data/EXR/M.USD.EUR.SP00.A");
+            responseFromBankXml.LoadXml(response.ToString());
+            string responseFromBankJson = JsonConvert.SerializeXmlNode(responseFromBankXml);
+            File.WriteAllText(@"C:\Temp\pankkijson.json", responseFromBankJson);
+            return;
         }
 
         // GET api/values/5
