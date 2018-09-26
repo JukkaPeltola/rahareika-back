48721395543ee4c77cab4612d8e47838078f4c89
melaaman <erika.laamanen@gmail.com>
2018-09-26 10:24:34 +0300

    get-metodi historicalrates lisätty. etsii historiallista dataa päivän mukaan

--- rahareika-back/Controllers/RatesController.cs
+++ rahareika-back/Controllers/RatesController.cs
@@ -1,17 +1,15 @@
 ﻿using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
+using System.Web.Http.Cors;
 using System.Web.Mvc;
 
 namespace rahareika_back.Controllers
-{
-
-    public class RatesController : Controller
+{    public class RatesController : Controller
     {
-
-        //HttpClient httpClient = new HttpClient();
         string url = "https://api.exchangeratesapi.io/latest";
-
-        public async Task<ActionResult> GetRates()
+        //string url2 = "https://api.exchangeratesapi.io/2010-01-12";
+        public async Task<ActionResult> GetLatestRates()
         {
             using (HttpClient client = new HttpClient())
             {
@@ -19,6 +17,31 @@ namespace rahareika_back.Controllers
                 client.DefaultRequestHeaders.Accept.Clear();
                 client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
                 HttpResponseMessage response = await client.GetAsync(url); 
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonData = await response.Content.ReadAsStringAsync();
+                    return (Content(jsonData, "application/json"));
+                }
+
+                return Json(1, JsonRequestBehavior.AllowGet);
+            }
+        }
+        public async Task<ActionResult> HistoricalRates(int id)
+        {
+            string date = id.ToString();
+            string year = date.Substring(0, 4);
+            string day = date.Substring(4, 2);
+            string month = date.Substring(6, 2);
+            string urlDate = year + "-" + day + "-" + month;
+
+            string newUrl = "https://api.exchangeratesapi.io/" + urlDate;
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new System.Uri(newUrl);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = await client.GetAsync(newUrl);
                 if (response.IsSuccessStatusCode)
                 {
                     string jsonData = await response.Content.ReadAsStringAsync();
