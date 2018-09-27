4b67627bef99d7c40ed006c1112bb3783285e19c
JukkaPeltola <jukka.peltola@mbnet.fi>
2018-09-27 14:54:44 +0300

    yhden valinnaisen valuutan haku edelliseltä kuulta ECBltä ja Jsonina ulostuuttaus toimii

--- rahareika-back/Controllers/ValuesController.cs
+++ rahareika-back/Controllers/ValuesController.cs
@@ -10,15 +10,17 @@ using Newtonsoft.Json;
 using System.IO;
 using System.Xml.Linq;
 using System.Web.Mvc;
+using rahareika_back.Models;
+using Newtonsoft;
 
 namespace rahareika_back.Controllers
 {
     public class ValuesController : ApiController
     {
         private static readonly HttpClient client = new HttpClient();
-        // GET api/values/{frequency}/{basecurrency}/{comparingcurrency}
+        // GET api/values/{comparingcurrency}
 
-        public string GetMonthlyUsdEurFrom1999ToNow(string basecurrency)
+        public string GetDailyEurCurrencyOfChoiceFromLast30Days(string basecurrency)
         {
 
             var inputUrl = $"https://sdw-wsrest.ecb.europa.eu/service/data/EXR/D.{basecurrency}.EUR.SP00.A?startPeriod="
@@ -77,12 +79,9 @@ namespace rahareika_back.Controllers
                         }
 
                     }
-                    string paluuStringi = "Tästä lähtee: " + System.Environment.NewLine;
-                    foreach (KeyValuePair<string, string> entry in record)
-                    {
-                        paluuStringi = paluuStringi + "Timestamp: " + entry.Key + " Value: " + entry.Value + System.Environment.NewLine;
-                    }
-                    return paluuStringi;
+                    EurToSmth values = new EurToSmth() { ComparingCurrency = basecurrency, TimeValuePairs = record };
+                    
+                    return JsonConvert.SerializeObject(values);
                 }
             }
             catch(Exception ex)
@@ -90,6 +89,80 @@ namespace rahareika_back.Controllers
                 return ex.ToString();
             }
         }
+        //// GET api/values/
+        //public string GetDailyEurToAllcurrenciesFromLast30Days()
+        //{
+
+        //    var inputUrl = $"https://sdw-wsrest.ecb.europa.eu/service/data/EXR/D..EUR.SP00.A?startPeriod="
+        //        + DateTime.Now.AddDays(-31).ToString("yyyy-MM-dd")
+        //        + "&endPeriod=" + DateTime.Now.AddDays(-1).ToString("yyyy-MM-dd");
+        //    try
+        //    {
+        //        using (XmlReader reader = XmlReader.Create(inputUrl))
+        //        {
+        //            //reader.ReadStartElement("generic:Series");
+        //            Dictionary<string, string> record = new Dictionary<string, string> { };
+
+        //            // variables for checking parse status 
+        //            string timestamp = "startvalue";
+        //            string valueInComparison = "startvalue";
+        //            string lastValue = "startvalue";
+        //            bool valueHasUpdated = false;
+
+        //            while (reader.Read())
+        //            {
+        //                //parse timestamps (ObsDimension) from currency observation timepoints and currency values in comparison (ObsValue) - "Obs" in ECB's SDMX hold these following values
+        //                if (reader.LocalName == "ObsDimension")
+        //                {
+        //                    while (reader.MoveToNextAttribute())
+        //                    {
+        //                        if (reader.LocalName != null || reader.LocalName != "")
+        //                        {
+        //                            if (reader.Value != null || reader.Value != "")
+        //                            {
+        //                                timestamp = reader.Value;
+        //                            }
+        //                        }
+        //                    }
+        //                }
+
+        //                if (reader.LocalName == "ObsValue")
+        //                {
+        //                    while (reader.MoveToNextAttribute())
+        //                    {
+        //                        if (reader.LocalName != null || reader.LocalName != "")
+        //                        {
+        //                            if (reader.Value != null || reader.Value != "")
+        //                            {
+        //                                valueInComparison = reader.Value;
+        //                                valueHasUpdated = true;
+        //                            }
+        //                        }
+        //                    }
+        //                }
+        //                // collect values to dictionary, when the reader has corresponding timestamp and currency comparison values
+        //                if (timestamp != "startvalue" && valueInComparison != "startvalue" && timestamp != lastValue && valueHasUpdated)
+        //                {
+        //                    record.Add(timestamp, valueInComparison);
+        //                    lastValue = timestamp;
+        //                    valueHasUpdated = false;
+        //                }
+
+        //            }
+        //            string paluuStringi = "Tästä lähtee: " + System.Environment.NewLine;
+        //            foreach (KeyValuePair<string, string> entry in record)
+        //            {
+        //                paluuStringi = paluuStringi + "Timestamp: " + entry.Key + " Value: " + entry.Value + System.Environment.NewLine;
+        //            }
+        //            return paluuStringi;
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return ex.ToString();
+        //    }
+        //}
+
         // GET api/values/5
         public string Get(int id)
         {
---
+++ rahareika-back/Models/EurToSmth.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace rahareika_back.Models
+{
+    public class EurToSmth
+    {
+        public string ComparingCurrency { get; set; }
+        public Dictionary <string, string> TimeValuePairs { get; set; }
+
+        
+    }
+
+}
